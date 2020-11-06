9a1e67eea032d759541adf35bc6c67052f271a2d
niokle <myszapl@gmail.com>
2020-11-06 11:00:00 +0100

    initial commit

---
+++ src/main/java/com/klenio/giswarnings/GisWarningsApplication.java
@@ -0,0 +1,13 @@
+package com.klenio.giswarnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisWarningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisWarningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klenio/giswarnings/GisWarningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klenio.giswarnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisWarningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
